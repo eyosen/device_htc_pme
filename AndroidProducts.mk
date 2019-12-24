raul-16/android_device_htc_pme

pme: bringup for LOS 17.0 aka Q/10
- TODO: bring back releasetools when its ready for verizon users.
( RIL and GPS not working without a proper blobs on these device's )

AndroidProducts.mk
@@ -15,3 +15,6 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_pme.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_pme-userdebug

eyosen/device_htc_pme

Change-Id: I901084c3a891fe8cb392fd23353d399e69874ed3

AndroidProducts.mk
@@ -15,3 +15,6 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aicp_pme.mk
+
+COMMON_LUNCH_CHOICES := \
+    aicp_pme-userdebug